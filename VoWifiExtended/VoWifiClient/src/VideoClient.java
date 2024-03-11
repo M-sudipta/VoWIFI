1e283b1886ebc53e09e0cbf9ab27b19239ec9396
M-sudipta <sudpitamohanty.1152002@gmail.com>
2024-03-12 02:03:19 +0530

    VoWIFI project

---
+++ VoWifiExtended/VoWifiClient/src/VideoClient.java
@@ -0,0 +1,34 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.highgui.HighGui;
+
+import java.io.*;
+import java.net.*;
+
+public class VideoClient {
+    static Socket socket;
+
+    public static void main(String[] args) {
+    	 System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        try {
+            socket = new Socket("192.168.154.165", 2323);
+
+            while (true) {
+            	//receiving
+                InputStream inStream = socket.getInputStream();
+                DataInputStream dataInputStream = new DataInputStream(inStream);
+                int length = dataInputStream.readInt();
+                byte[] buffer = new byte[length];
+                dataInputStream.readFully(buffer, 0, buffer.length);
+                //display
+                Mat frame = Imgcodecs.imdecode(new MatOfByte(buffer), Imgcodecs.IMREAD_UNCHANGED);
+                HighGui.imshow("Host's Video", frame);
+                HighGui.waitKey(1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
